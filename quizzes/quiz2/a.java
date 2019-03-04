4f44d14402ca24432fb9dc03354fdbcb64d3a552
dsalinasduron <dsalinasduron@westminstercollege.edu>
2019-03-04 16:49:30 -0700

    add quiz2 study guide

---
+++ quizzes/quiz2/a.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class a {
+	public static void main(String[] args) {
+		ArrayList<Integer> a = new ArrayList();
+		ArrayList b = new ArrayList<Integer>();
+		b.add("ada");
+		Object x = new String("a");
+		System.out.println(x.charAt(0));
+	}
+}
