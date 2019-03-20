b8706856d7b1cd6dcc9ff7b456adeede4e53dbb8
dsalinasduron <dsalinasduron@westminstercollege.edu>
2019-03-20 14:53:26 -0600

    make GraphExample.java more legible

--- labs/lab9/GraphExample.java
+++ labs/lab9/GraphExample.java
@@ -1,10 +1,19 @@
 import java.util.List;
 /* This example class uses a class called Node to represent each vertex and a
- * a list with two nodes in it to represent each edge.*/
-public class GraphExample implements UndirectedGraph<Node,List<Node>>{
-	public boolean isComplete() {return false;}
-	public boolean hasCycle() {return false;}
-	public List<Node> getVertices() {return null;}
-	public List<List<Node>> getEdges() {return null;}
+ * list with two nodes in it to represent each edge. That is why Vertex and
+ * Edge have been replaced with Node and List<Node>, respectively.*/
+public class GraphExample implements UndirectedGraph< Node, List<Node> > {
+	public boolean isComplete() {
+		return false;
+	}
+	public boolean hasCycle() {
+		return false;
+	}
+	public List<Node> getVertices() {
+		return null;
+	}
+	public List<List<Node>> getEdges() {
+		return null;
+	}
 
 }
